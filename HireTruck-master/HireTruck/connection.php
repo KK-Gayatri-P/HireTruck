13a154ee0559ef416283d196f24e626c49c08915
KK-Gayatri-P <kkpg2014.pk@gmail.com>
2020-04-20 23:05:20 +0530

    HireTruck-master

--- HireTruck-master/HireTruck/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
- $con=mysqli_connect("localhost","root","","hire_truck_demo");
-
- if($con)
- {
-	 //echo "successfully";
- }
-
-?>
--- HireTruck/HireTruck/Admin/session.php
+++
@@ -1,7 +0,0 @@
-<?php
-require("../connection.php");
-@session_start();
-if(!isset($_SESSION['mail']) && !isset($_SESSION['user_type'])){
-    header( "location:adm_login.php");
-}
-?>
